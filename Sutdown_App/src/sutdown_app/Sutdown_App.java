e8d0ae263baab27605b5e37d8252ded91cf0fe0b
Ahmed ElSayed <AhmedElSayed3210@users.noreply.github.com>
2018-08-31 16:14:47 +0200

    shutdown_app
    shutdown pc application using java

---
+++ Sutdown_App/src/sutdown_app/Sutdown_App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sutdown_app;
+
+/**
+ *
+ * @author Ahmed El_Sayed
+ */
+public class Sutdown_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        shutdownapp sh=new shutdownapp();
+        sh.setVisible(true);
+    }
+    
+}
